d5aacd108fddf932d23ce2f3c848b1bc085aedb7
Victor De la Torre <virata606@gmail.com>
2022-04-03 19:13:51 -0500

    Initial Commit

---
+++ MyMovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMovieApp
+//
+//  Created by Victor De la Torre Anicama on 3/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
